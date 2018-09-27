98061006960dfb2250d4ba5013553b2835ac84f3
moluram <moluram.a@gmail.com>
2018-09-27 06:57:55 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let levels = []; 
+    
+  for (br of str) {
+    let brackets = bracketsConfig.find(bs => bs.some(b => b === br))
+    
+    if (brackets[0] === brackets[1] && levels.some(l => l === bracketsConfig.indexOf(brackets))) {
+      let level = levels.pop();
+      if (level != bracketsConfig.indexOf(brackets) || typeof level == 'undefined') {
+        return false;
+      } 
+
+    } else if (br === brackets[0]) {
+
+      levels.push(bracketsConfig.indexOf(brackets));
+
+    } else {
+
+      let level = levels.pop();
+
+      if (typeof level == 'undefined' || bracketsConfig[level][1] !== brackets[1]) {
+        return false;
+      }
+    }
+  }
+
+  return levels.length === 0;
 }
